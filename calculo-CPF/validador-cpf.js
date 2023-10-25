c1d18ddd332560f260c4a0bdf3d94187fb343de6
Heitor Alves Pinto <134733931+HeitorAlvesP@users.noreply.github.com>
2023-10-25 11:07:51 -0300

    Update validador-cpf.js
    Aprendi a usar class e static

--- calculo-CPF/validador-cpf.js
+++ calculo-CPF/validador-cpf.js
@@ -1,49 +1,53 @@
-function ValidaCPF(cpfEnviado){
-    Object.defineProperty(this, 'cpfLimpo',{
-        get: function(){
-            return cpfEnviado.replace(/\D+/g, '')
+// 705.484.450-52   070.987.720-03 ----CPF para testes----
+class ValidaCPF{
+    constructor(cpfEnviado){
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: false,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        })
+    }
+
+    eSequencia(){
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
+    }
+
+    geraNovoCpf(){
+        const cpfSemDigito = this.cpfLimpo.slice(0, -2)
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigito);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigito + digito1);
+        this.novoCpf = cpfSemDigito + digito1 + digito2
+    }
+
+    static geraDigito(cpfSemDigito){
+        let total = 0;
+        let reverso = cpfSemDigito.length + 1
+
+        for(let stringNumerica of cpfSemDigito){
+            total += reverso * Number(stringNumerica); 
+            reverso --
         }
-    });
+
+        const digito = 11 - (total % 11)
+        return digito <= 9 ? String(digito) : '0'
+    }
+
+    valida(){
+        if(!this.cpfLimpo) return false;
+        if(typeof this.cpfLimpo !== 'string') return false
+        if(this.cpfLimpo.length !== 11) return false
+        if(this.eSequencia()) return false
+        this.geraNovoCpf()
+        console.log(this.novoCpf)
+
+        return this.novoCpf === this.cpfLimpo
+    }
 }
 
-
-ValidaCPF.prototype.valida = function(){
-    if( typeof this.cpfLimpo === 'undefined') return false
-    if(this.cpfLimpo.length !== 11) return false;
-    if(this.isSequencia()) return false;
-
-    const cpfParcial = this.cpfLimpo.slice(0, -2);
-    const digito1 = this.criaDigito(cpfParcial);
-    const digito2 = this.criaDigito(cpfParcial + digito1);
-
-    const novoCpf = cpfParcial + digito1 + digito2
-    return novoCpf === this.cpfLimpo;
-}
-
-
-ValidaCPF.prototype.criaDigito = function(cpfParcial){
-    const cpfArray = Array.from(cpfParcial);
-
-    let regressivo = cpfArray.length + 1;
-    let total = cpfArray.reduce((ac, val) => {
-        ac += (regressivo * Number(val))
-        regressivo --
-        return ac;
-    }, 0);
-
-    const digito = 11 - (total % 11);
-    return digito > 9 ? '0' : String(digito);
-}
-
-ValidaCPF.prototype.isSequencia = function(){
-    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
-    return sequencia === this.cpfLimpo
-}
-
-const cpf = new ValidaCPF('***.***.***-**'); //Insira um Cpf 
-
-if(cpf.valida()){
-    console.log('Cpf válido')
-} else {
-    console.log ('Cpf inválido')
+let validaCpf = new ValidaCPF('070.987.720-03') //COLOQUE SEU CPF AQUI
+if(validaCpf.valida()){
+    console.log('CPF válido')
+}else{
+    console.log('CPF inválido')
 }
